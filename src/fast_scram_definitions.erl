ad3b3cf7e64d955d0e2b4497edefd1f238357743
Nelson Vides <videsnelson@gmail.com>
2020-08-26 18:29:47 +0200

    Fill all possible scram definitions
    Given a `#scram_defintions{}` record and supplemental data, try to fill
    all the possible and necessary gaps. This supplemental data will include
    passwords, cached salts and iteration counts.

--- src/fast_scram_definitions.erl
+++ src/fast_scram_definitions.erl
@@ -12,6 +12,10 @@
          server_signature/3
         ]).
 
+-export([
+         scram_definitions_pipe/3
+        ]).
+
 %%%===================================================================
 %%% SCRAM Definitions
 %%%===================================================================
@@ -68,3 +72,94 @@ crypto_hmac(Sha, Bin1, Bin2) ->
 crypto_hmac(Sha, Bin1, Bin2) ->
     crypto:hmac(Sha, Bin1, Bin2).
 -endif.
+
+-spec scram_definitions_pipe(scram_definitions(), challenge(), map()) ->
+    scram_definitions().
+%% Typical scenario, auth_message is full and we only have the plain password
+scram_definitions_pipe(
+  #scram_definitions{hash_method = HashMethod,
+                     salted_password = <<>>,
+                     auth_message = AuthMessage} = Scram,
+  #challenge{salt = Salt, it_count = ItCount},
+  #{password := Password}) when AuthMessage =/= <<>> ->
+    SaltedPassword = salted_password(HashMethod, Password, Salt, ItCount),
+    ClientKey = client_key(HashMethod, SaltedPassword),
+    StoredKey = stored_key(HashMethod, ClientKey),
+    ClientSignature = client_signature(HashMethod, StoredKey, AuthMessage),
+    ClientProof = client_proof(ClientKey, ClientSignature),
+    ServerKey = server_key(HashMethod, SaltedPassword),
+    ServerSignature = server_signature(HashMethod, ServerKey, AuthMessage),
+    Scram#scram_definitions{
+       salted_password = SaltedPassword,
+       client_key = ClientKey,
+       stored_key = StoredKey,
+       auth_message = AuthMessage,
+       client_signature = ClientSignature,
+       client_proof = ClientProof,
+       server_key = ServerKey,
+       server_signature = ServerSignature
+      };
+%% We have a cached challenge, we need to verify if it is correct
+scram_definitions_pipe(
+  #scram_definitions{hash_method = HashMethod, auth_message = AuthMessage} = Scram,
+  #challenge{} = GivenChallenge,
+  #{challenge := StoredChallenge} = Data) ->
+    case GivenChallenge =:= StoredChallenge of
+        true -> % This means that the client has cached the challenge correctly
+            partial_compute(Scram);
+        false -> % Invalid cache, remove all knowledge and try again
+            ScramWithoutCached = #scram_definitions{hash_method = HashMethod, auth_message = AuthMessage},
+            DataWithoutCached = maps:remove(challenge, Data),
+            scram_definitions_pipe(ScramWithoutCached, GivenChallenge, DataWithoutCached)
+    end;
+scram_definitions_pipe(Scram, _, _) ->
+    partial_compute(Scram).
+
+partial_compute(Scram =
+  #scram_definitions{
+     hash_method = HashMethod, auth_message = AuthMessage,
+     client_key = ClientKey, server_key = ServerKey
+    }) when ClientKey =/= <<>>, ServerKey =/= <<>> ->
+    StoredKey = stored_key(HashMethod, ClientKey),
+    ClientSignature = client_signature(HashMethod, StoredKey, AuthMessage),
+    ClientProof = client_proof(ClientKey, ClientSignature),
+    ServerSignature = server_signature(HashMethod, ServerKey, AuthMessage),
+    Scram#scram_definitions{
+      stored_key = StoredKey,
+      client_signature = ClientSignature,
+      client_proof = ClientProof,
+      server_signature = ServerSignature
+     };
+partial_compute(Scram =
+  #scram_definitions{
+     hash_method = HashMethod, auth_message = AuthMessage,
+     stored_key = StoredKey, server_key = ServerKey
+    }) when StoredKey =/= <<>>, ServerKey =/= <<>> ->
+    ClientSignature = client_signature(HashMethod, StoredKey, AuthMessage),
+    ServerSignature = server_signature(HashMethod, ServerKey, AuthMessage),
+    Scram#scram_definitions{
+      client_signature = ClientSignature,
+      server_signature = ServerSignature
+     };
+partial_compute(Scram =
+  #scram_definitions{
+     hash_method = HashMethod, auth_message = AuthMessage,
+     salted_password = SaltedPassword
+    }) when SaltedPassword =/= <<>> ->
+    ClientKey = client_key(HashMethod, SaltedPassword),
+    StoredKey = stored_key(HashMethod, ClientKey),
+    ClientSignature = client_signature(HashMethod, StoredKey, AuthMessage),
+    ClientProof = client_proof(ClientKey, ClientSignature),
+    ServerKey = server_key(HashMethod, SaltedPassword),
+    ServerSignature = server_signature(HashMethod, ServerKey, AuthMessage),
+    Scram#scram_definitions{
+       client_key = ClientKey,
+       stored_key = StoredKey,
+       client_signature = ClientSignature,
+       client_proof = ClientProof,
+       server_key = ServerKey,
+       server_signature = ServerSignature
+      };
+partial_compute(Scram) ->
+    ?LOG_DEBUG(#{what => scram_no_pipe_match}),
+    Scram.
