466691cba50d9179ea03060a7c0ba4841e97addd
Nelson Vides <nelson.vides@erlang-solutions.com>
2021-02-05 16:07:38 +0100

    Implement the full pbkdf2 algoritm
    Inspiration from https://github.com/riverrun/pbkdf2_elixir ;)

--- src/fast_scram.erl
+++ src/fast_scram.erl
@@ -16,7 +16,7 @@
     {continue,  next_message(), fast_scram_state()} |
     {error,    error_message(), fast_scram_state()}.
 
--export([hi/4, pbkdf2_block/5]).
+-export([hi/4, pbkdf2_block/5, pbkdf2/5]).
 
 -export([mech_new/1,
          mech_step/2
@@ -338,6 +338,17 @@ hi(Hash, Password, Salt, IterationCount) ->
 -spec pbkdf2_block(sha_type(), binary(), binary(), non_neg_integer(), non_neg_integer()) -> binary().
 pbkdf2_block(_Hash, _Password, _Salt, _IterationCount, _BlockSize) ->
     erlang:nif_error(not_loaded).
+
+-spec pbkdf2(sha_type(), binary(), binary(), non_neg_integer(), non_neg_integer()) -> binary().
+pbkdf2(Hash, Password, Salt, IterationCount, DkLen) ->
+    pbkdf2(Hash, Password, Salt, IterationCount, DkLen, 1, [], 0).
+
+pbkdf2(_Hash, _Password, _Salt, _IterationCount, DkLen, _BlockIndex, Acc, Len) when Len >= DkLen ->
+    Bin = iolist_to_binary(lists:reverse(Acc)),
+    binary:part(Bin, 0, DkLen);
+pbkdf2(Hash, Password, Salt, IterationCount, DkLen, BlockIndex, Acc, Len) ->
+    Block = pbkdf2_block(Hash, Password, Salt, IterationCount, BlockIndex),
+    pbkdf2(Hash, Password, Salt, IterationCount, DkLen, BlockIndex + 1, [Block | Acc], byte_size(Block) + Len).
 
 -spec load() -> any().
 load() ->
