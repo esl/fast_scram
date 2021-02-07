c813aff8a56dd1556961917f562d33c53dda56d5
Nelson Vides <nelson.vides@erlang-solutions.com>
2021-02-07 12:54:35 +0100

    HMAC trick in tests
    `crypto:hmac` was deprecated and it's going to be removed soon, and so
    logs complain a lot about using so in modern versions of OTP. So do in
    tests the same macro trick than in source.

--- test/erlang_scram.erl
+++ test/erlang_scram.erl
@@ -6,17 +6,17 @@
 
 -spec hi(sha_type(), binary(), binary(), non_neg_integer()) -> binary().
 hi(Sha, Password, Salt, 1) ->
-    crypto:hmac(Sha, Password, <<Salt/binary, 0, 0, 0, 1>>);
+    crypto_hmac(Sha, Password, <<Salt/binary, 0, 0, 0, 1>>);
 hi(Sha, Password, Salt, IterationCount)
   when is_integer(IterationCount), IterationCount > 1 ->
-    U1 = crypto:hmac(Sha, Password, <<Salt/binary, 0, 0, 0, 1>>),
+    U1 = crypto_hmac(Sha, Password, <<Salt/binary, 0, 0, 0, 1>>),
     mask(U1, hi_round(Sha, Password, U1, IterationCount - 1)).
 
 -spec hi_round(sha_type(), binary(), binary(), non_neg_integer()) -> binary().
 hi_round(Sha, Password, UPrev, 1) ->
-    crypto:hmac(Sha, Password, UPrev);
+    crypto_hmac(Sha, Password, UPrev);
 hi_round(Sha, Password, UPrev, IterationCount) ->
-    U = crypto:hmac(Sha, Password, UPrev),
+    U = crypto_hmac(Sha, Password, UPrev),
     mask(U, hi_round(Sha, Password, U, IterationCount - 1)).
 
 -spec mask(binary(), binary()) -> binary().
@@ -26,3 +26,11 @@ mask(Key, Data) ->
     <<B:KeySize>> = Data,
     C = A bxor B,
     <<C:KeySize>>.
+
+-if(?OTP_RELEASE >= 22).
+crypto_hmac(Sha, Bin1, Bin2) ->
+    crypto:mac(hmac, Sha, Bin1, Bin2).
+-else.
+crypto_hmac(Sha, Bin1, Bin2) ->
+    crypto:hmac(Sha, Bin1, Bin2).
+-endif.
